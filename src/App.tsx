9b4b0684f386b927facfdb23fcedd0f7d260000a
Paskinho <v.paskanov@gmail.com>
2023-07-30 15:25:45 +0300

    Some changes in project with state and more

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import { Test } from './features/test/Test';
 
 function App() {
+
+  const [state, setState] = useState({startValue: 0, maxValue: 100, currentValue: 0})
+
+  const setValue = () => {
+    setState({...state,currentValue: state.currentValue } )
+  }
+
+  const addBlock = () => {
+    let block = 100
+  }
+
   return (
     <div className="App">
     <Test/>
--- src/features/test/Test.tsx
+++ src/features/test/Test.tsx
@@ -1,24 +1,36 @@
-import {useState} from "react";
-// import {Slider} from "@mui/material";
+import {ChangeEvent, useState} from "react";
+import s from "./Test.module.css"
+import {Slider} from "@mui/material";
 
 export const Test = () => {
 
-    const [test, setTest] = useState(0)
+    const [value, setValue] = useState(0)
 
     const saveDimensions = () => {
+
         console.log("success")
     }
 
 return (
     <div>
+        <form>
     <div>
-        <input placeholder={'length'}/>
+        <input placeholder={'length'} value={value}
+               onChange={(event: ChangeEvent<HTMLInputElement>)=>{
+            setValue(+event.currentTarget.value)}}
+        />
     </div>
-        {/*<div>*/}
-        {/*    <Slider/>*/}
+        {/*<div className = {s.slider}>*/}
+        {/*    <Slider onChange={saveDimensions}/>*/}
         {/*</div>*/}
         <div>
     <button onClick={saveDimensions}>Save</button>
+        </div>
+        </form>
+        <div className={s.block}>
+            {value}
+        </div>
+        <div className={s.line}>
         </div>
     </div>
 )
