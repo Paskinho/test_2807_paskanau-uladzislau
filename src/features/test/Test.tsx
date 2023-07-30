985f3a5e185d34434d2bb7510b7f777cfb19d78c
Paskinho <v.paskanov@gmail.com>
2023-07-30 10:42:19 +0300

    Preparation for create project

--- src/features/test/Test.tsx
+++ src/features/test/Test.tsx
@@ -1,16 +1,24 @@
 import {useState} from "react";
+// import {Slider} from "@mui/material";
 
 export const Test = () => {
 
     const [test, setTest] = useState(0)
+
+    const saveDimensions = () => {
+        console.log("success")
+    }
 
 return (
     <div>
     <div>
         <input placeholder={'length'}/>
     </div>
+        {/*<div>*/}
+        {/*    <Slider/>*/}
+        {/*</div>*/}
         <div>
-    <button>Save</button>
+    <button onClick={saveDimensions}>Save</button>
         </div>
     </div>
 )
