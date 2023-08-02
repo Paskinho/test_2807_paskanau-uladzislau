7a0b36c909b51600c9995baf1d3c258585078353
Paskinho <v.paskanov@gmail.com>
2023-08-02 19:51:05 +0300

    remove blocks accepted

--- src/features/test/Test.tsx
+++ src/features/test/Test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {ChangeEvent, useState} from "react";
 import s from "./Test.module.css"
 import {Divider, inputClasses, Slider} from "@mui/material";
-import {v1} from 'uuid';
+import { v1 } from 'uuid';
 
 
 // type TestPropsType = {
@@ -21,20 +21,20 @@ export const Test = () => {
     const [length, setLength] = useState(0)
 
 
-    const [inputs, setInputs] = useState([[
+    const [inputs, setInputs] = useState([
         {id: v1(), input: <input size={0} className={s.input}/>}
         // <input size={0} className={s.input}/>,
-    ]])
+    ])
 
 
     const removeBlock = (id: any) => {
 
-        // let filteredInputs = inputs.filter(input => input.id != id)
-
+        let filteredInputs = inputs.filter(input => input.id !== id)
+        setInputs(filteredInputs)
         // delete inputs[id]
         // delete inputs[id]
         // setInputs(inputs.filter(il => il))
-         return inputs.splice(0,id) // уточнять
+        //  return inputs.splice(0,id) // уточнять
         // console.log('delete success')
     }
 
@@ -43,13 +43,13 @@ export const Test = () => {
 
 
     const addInput = (length: number) => {
-        let input = [{id: v1(), input:<input className={s.backlightInput}
+        let input = {id: v1(), input:<input className={s.backlightInput}
                             value={fixed}
                             onChange={(event: ChangeEvent<HTMLInputElement>) => {
                                 setLength(+event.currentTarget.value)
                             }}
                             onDoubleClick={removeBlock}
-                            size={length}/>}]
+                            size={length}/>}
         let newInput = [...inputs, input]
         setInputs(newInput)
     }
@@ -72,7 +72,7 @@ export const Test = () => {
                 </button>
             </div>
             <div className={s.block}>
-                [{inputs.map(inputs)}]
+                {inputs.map(tl => tl.input)}
             </div>
             <div className={s.line}>
             </div>
